4d271b3164640283821b58617b2255bbab0f3424
Lisha Kuinkel <ltkuinkel@gmail.com>
2023-07-27 19:05:52 -0600

    added some handlebars code

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3001
+const path = require('path');
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.send('Hello from Pizza Express!')
