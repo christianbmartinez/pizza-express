7fe337e12a54edd4dbf46b46acf63fba687bd87b
Christian Martinez <christian@mixdlabs.com>
2023-07-30 14:40:46 -0600

    Created a carousel for the pizza products on the /order page

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -6,3 +6,28 @@ const logout = async () => {
   })
   window.location.replace('/login')
 }
+// Carousel Slider
+let pixels = 0
+
+if (window.innerWidth < 780) {
+  pixels = 0
+} else {
+  pixels = -300
+}
+
+const goNext = () => {
+  pixels = pixels - 467
+  const slider = document.getElementById('slider')
+  if (Math.abs(pixels) >= slider.scrollWidth / 1.7) {
+    pixels = 0
+  }
+  slider.style.transform = `translateX(${pixels}px)`
+}
+
+const goPrev = () => {
+  const slider = document.getElementById('slider')
+  if (Math.abs(pixels) === 0) {
+    pixels = 0
+  }
+  slider.style.transform = `translateX(${pixels}px)`
+}
