0e23f01a50c409f305cab308ffcb311138a2492c
Christian Martinez <christian@mixdlabs.com>
2023-07-30 12:42:46 -0600

    Added checkout route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -10,6 +10,11 @@ router.get('/order', (req, res) => {
   res.render('order')
 })
 
+// Checkout route
+router.get('/checkout', (req, res) => {
+  res.render('checkout')
+})
+
 // Login route
 router.get('/login', (req, res) => {
   res.render('login')
