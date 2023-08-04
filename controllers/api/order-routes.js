3c48e21d5c18e04427d9a3e764db230c3a6bbe4a
Lisha Kuinkel <ltkuinkel@gmail.com>
2023-08-03 20:58:58 -0600

    fixed some code errors

--- controllers/api/order-routes.js
+++ controllers/api/order-routes.js
@@ -3,23 +3,29 @@ const { Payment, Pizza, Toppings, User, Order } = require('../../models')
 
 router.get('/item/:id', async (req, res) => {
   if (req.session.logged_in) {
-  try {
-    const getPizzabyId = await Pizza.findOne({ where: { id: req.params.id } });
-    
-    const pizza = getPizzabyId.get({plain:true});
-    if (!pizza) {
-      res.status(404).json({ message: 'no pizza found with that id' });
-      return;
-    }
+    try {
+      const getPizzabyId = await Pizza.findOne({ where: { id: req.params.id } });
+      const getToppings = await Toppings.findAll({ attributes: ['name'] })
 
-    res.render('order-item', { pizza, logged_in: true });
-  } catch (err) {
-    res.status(500).json(err);
+      const pizza = getPizzabyId.get({ plain: true });
+      console.log(pizza);
+      const toppingData = getToppings.map((toppings) => toppings.dataValues)
+      console.log(toppingData);
+
+      if (!pizza) {
+        res.status(404).json({ message: 'no pizza found with that id' });
+        return;
+      }
+
+      res.render('order-item', { pizza, Toppings, logged_in: true });
+
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
-}
   else {
-    res.render('login')
-  
+    res.render('login');
+
   }
 })
 
@@ -27,7 +33,7 @@ router.post('/', async (req, res) => {
   try {
     const newOrder = await Order.create({
       ...req.body,
-      user_id: req.session.user_id,
+      user_id: req.session.user_id
     });
 
     res.status(200).json(newOrder);
